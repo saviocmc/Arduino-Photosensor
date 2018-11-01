c37a1831405b1b6761ed8b63c039e4063ea570e0
Gilles Schintgen <gschintgen@posteo.de>
2018-11-01 14:03:00 +0100

    update and move comment about connection type

--- src/Photosensor.h
+++ src/Photosensor.h
@@ -6,18 +6,16 @@
 #define BRIGHT 1
 #define DARK 0
 
-/**
-* One terminal of the sensor have to be connected on an analog pin.
-* The other one can be connected on GND or 5V (through a resistor).
-* Leave the correct #define below depending on how did you connect the sensor on your circuit.
-*/
-
 #include "Arduino.h"
 
 /**
 * Represents a Light-Dependent Resistor (LDR) - Photoresistor.
 * It gives usable both digital and analog information about the ambient luminosty.
 * As well it saves and compares data from different readings to provide information about changes in ambient luminosity.
+*
+* One terminal of the sensor have to be connected on an analog pin.
+* The other one can be connected on GND or 5V (through a resistor).
+* This needs to be specified when calling the class constructor.
 */
 class Photosensor {
 
